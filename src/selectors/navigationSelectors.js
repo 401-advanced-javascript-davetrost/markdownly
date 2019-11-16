9952dd3d8f31f668e0ad3587c7fc9fe836869fe5
olliecomet <leyechi@gmail.com>
2019-11-15 17:44:04 -0800

    refactored navigation component

--- src/selectors/navigationSelectors.js
+++ src/selectors/navigationSelectors.js
@@ -4,6 +4,5 @@ export const getFileToDelete = state => state.navigation.fileToDelete;
 export const getUpdatedMarkdown = state => state.documents.markdown;
 export const getMarkdown = state => state.navigation.files[state.navigation.activeDocument].markdown;
 export const getTitleSearchTerm = state => state.navigation.titleSearchTerm;
-export const getFileToOpen = state => state.navigation.fileToOpen;
 export const getMatchingFiles = state => state.navigation.matchingFiles;
 
