af6e6ea8aa19abbf4c1359c2e0b8d56cde203f22
olliecomet <leyechi@gmail.com>
2019-11-15 16:56:48 -0800

    refactored document comp

--- src/components/markdown/Document.js
+++ src/components/markdown/Document.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMarkdown } from '../../selectors/navigationSelectors';
+import { updateMarkdown } from '../../actions/navigationActions';
 import Preview from './Preview';
 import Editor from './Editor';
 import styles from './Document.css';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { getMarkdown } from '../../selectors/navigationSelectors';
-import { updateMarkdown } from '../../actions/navigationActions';
 
-const Document = ({
-  markdown,
-  handleChange
-}) => {
+export default function Document() {
+  const markdown = useSelector(getMarkdown);
+  const dispatch = useDispatch();
+
+  const handleChange = ({ target }) => dispatch(updateMarkdown(target.value));
+
   return (
     <>
       <div className={styles.Document}>
@@ -19,24 +20,4 @@ const Document = ({
       </div>
     </>
   );
-};
-
-Document.propTypes = {
-  markdown: PropTypes.string.isRequired,
-  handleChange: PropTypes.func.isRequired
-};
-
-const mapStateToProps = state => ({
-  markdown: getMarkdown(state)
-});
-
-const mapDispatchToProps = dispatch => ({
-  handleChange({ target }) {
-    dispatch(updateMarkdown(target.value));
-  }
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Document);
+}
